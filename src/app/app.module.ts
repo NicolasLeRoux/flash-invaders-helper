745ecb93caa3b9e4360b49bd9b1305dc5b4e2949
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-08-16 19:05:36 +0200

    feat(Material Design): Add angular material lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule, MatSidenavModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared';
@@ -11,8 +13,11 @@ import { SharedModule } from './shared';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    SharedModule.forRoot()
+    SharedModule.forRoot(),
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
