a4f4ae1f81dfc7ad3367a71d8e9bb47dddf46389
Nicolas Le Roux <mr.leroux.nicolas@gmail.com>
2018-08-21 13:31:33 +0200

    feat(Leafet): Add leaflet on the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from './shared/services/account.service';
 import { FlashesService } from './shared/services/flashes.service';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import * as Leaflet from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -22,14 +23,29 @@ export class AppComponent implements OnInit {
   public ngOnInit() {
     this.iconRegistrySrv.addSvgIconSet(this.sanitizer.bypassSecurityTrustResourceUrl('./assets/icon-set.svg'));
 
-    this.accountSrv.query('')
+    /*this.accountSrv.query('')
       .subscribe(data => {
         console.log(data);
-      });
+      });*/
 
-    this.flashesSrv.query('')
+    /*this.flashesSrv.query('')
       .subscribe(data => {
         console.log(data);
-      });
+      });*/
+
+    const mymap = Leaflet.map('app-map', {
+      center: [51.505, -0.09],
+      zoom: 13,
+      zoomControl: false
+    });
+
+    Leaflet.marker([51.5, -0.09]).addTo(mymap);
+
+    Leaflet.circle([51.508, -0.11], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 500
+    }).addTo(mymap);
   }
 }
